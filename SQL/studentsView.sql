cd8f7d628aa0a63112e4bbaf318e9f3d1026db8e
Nick <Nick@10.20.204.70>
2014-09-24 21:58:46 +0800

    Completely finished studentsView.sql and markersView.sql

--- SQL/studentsView.sql
+++ SQL/studentsView.sql
@@ -1,20 +1,18 @@
 DROP VIEW IF EXISTS seminarmarks.studentsView;
 CREATE VIEW seminarmarks.studentsView AS
 SELECT 	CONCAT(StudentFirstName, ' ', StudentLastName) AS StudentName,
-		StudentNumber,
-		AVG(ProposalMarks.Mark1) AS ProposalMark1,
-		AVG(ProposalMarks.Mark2) AS ProposalMark2,
-		AVG(ProposalMarks.Mark3) AS ProposalMark3,
-		AVG(ProposalMarks.Mark1) + AVG(ProposalMarks.Mark2) + AVG(ProposalMarks.Mark3) AS ProposalTotal,
-		AVG(FinalMarks.Mark1) AS FinalMark1,
-		AVG(FinalMarks.Mark2) AS FinalMark2,
-		AVG(FinalMarks.Mark3) AS FinalMark3,
-		AVG(FinalMarks.Mark1) + AVG(FinalMarks.Mark2) + AVG(FinalMarks.Mark3) AS FinalTotal,
-		AVG(ProposalMarks.Mark1) + AVG(ProposalMarks.Mark2) + AVG(ProposalMarks.Mark3) + AVG(FinalMarks.Mark1) + AVG(FinalMarks.Mark2) + AVG(FinalMarks.Mark3) as Total
+        StudentNumber,
+        AVG(ProposalMarks.Mark1) AS ProposalMark1,
+        AVG(ProposalMarks.Mark2) AS ProposalMark2,
+        AVG(ProposalMarks.Mark3) AS ProposalMark3,
+        AVG(ProposalMarks.Mark1) + AVG(ProposalMarks.Mark2) + AVG(ProposalMarks.Mark3) AS ProposalTotal,
+        AVG(FinalMarks.Mark1) AS FinalMark1,
+        AVG(FinalMarks.Mark2) AS FinalMark2,
+        AVG(FinalMarks.Mark3) AS FinalMark3,
+        AVG(FinalMarks.Mark1) + AVG(FinalMarks.Mark2) + AVG(FinalMarks.Mark3) AS FinalTotal,
+        AVG(ProposalMarks.Mark1) + AVG(ProposalMarks.Mark2) + AVG(ProposalMarks.Mark3) + AVG(FinalMarks.Mark1) + AVG(FinalMarks.Mark2) + AVG(FinalMarks.Mark3) as Total
 FROM students 
 INNER JOIN marks as ProposalMarks ON students.idStudent=ProposalMarks.idStudent
 INNER JOIN marks as FinalMarks ON students.idStudent=FinalMarks.idStudent
 WHERE ProposalMarks.seminar=1 AND FinalMarks.seminar=2
 GROUP BY students.idStudent;
-
-select * from studentsView;
