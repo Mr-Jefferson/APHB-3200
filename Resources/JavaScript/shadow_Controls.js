bfd3a3d6d3c7615ae38497727e43f99b3798a716
Gimbo44 <Gimblett44@gmail.com>
2014-10-01 18:20:01 +0800

    Merge origin/master
    Conflicts:
    	Library/Helpers/Page.php
    	Resources/StyleSheet/Project_StyleSheet.css

--- Resources/JavaScript/shadow_Controls.js
+++ Resources/JavaScript/shadow_Controls.js
@@ -1,11 +1,11 @@
 function display() {
     document.getElementById('shadow_add_student').style.display = "none";
     document.getElementById('shadow_add_marker').style.display = "none";
-    
+
     document.getElementById('hidden_form').style.display = "block";
     document.getElementById('shadow_add_'+arguments[0]).style.display = "block";
 }
 
 function Hide() {
-    document.getElementById('hidden_form').style.display = "none";
+     document.getElementById('hidden_form').style.display = "none";
 }
