5fb4cd766420100710b0326f00cf1452a141a072
Nick <Nick@NickLaptop>
2014-10-16 15:24:38 +0800

    Bug hotfix, final hotfix, everything appears to be fine

--- SQL/students_overall_stats.sql
+++ SQL/students_overall_stats.sql
@@ -2,7 +2,7 @@ USE seminar_marks;
 DROP VIEW IF EXISTS students_overall_stats;
 
 CREATE VIEW students_overall_stats AS
-SELECT	AVG(proposal_total) AS overall_average,
+(SELECT	AVG(proposal_total) AS overall_average,
 		MAX(proposal_total) AS overall_maximum,
 		MIN(proposal_total) AS overall_minimum,
 		(MAX(proposal_total)-MIN(proposal_total)) as overall_range,
@@ -11,8 +11,10 @@ SELECT	AVG(proposal_total) AS overall_average,
 		students.semester,
 		1 as seminar
 FROM	students INNER JOIN
-		students_overall ON students_overall.id_student = students.id_student UNION
-SELECT	AVG(final_total) AS overall_average,
+		students_overall ON students_overall.id_student = students.id_student
+GROUP BY semester, cohort)
+UNION
+(SELECT	AVG(final_total) AS overall_average,
 		MAX(final_total) AS overall_maximum,
 		MIN(final_total) AS overall_minimum,
 		(MAX(final_total)-MIN(final_total)) as overall_range,
@@ -21,4 +23,7 @@ SELECT	AVG(final_total) AS overall_average,
 		students.semester,
 		2 as seminar
 FROM	students INNER JOIN
-		students_overall ON students_overall.id_student = students.id_student;
+		students_overall ON students_overall.id_student = students.id_student
+GROUP BY semester, cohort);
+
+select * from students_overall_stats
