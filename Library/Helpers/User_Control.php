e66319c1cb9027485ea1821e8f9e8c47c65e6431
Gimbo44 <gimblett44@gmail.com>
2014-10-17 03:39:01 +0800

    Comments

--- Library/Helpers/User_Control.php
+++ Library/Helpers/User_Control.php
@@ -5,18 +5,15 @@ Class User_Control {
 
     protected $DB_connection;
 
-    /**
-     *  Username/Password paramaters will come from html forms via the  post method
-     * @param string $username  
-     * @param string $password
-     */
     public function __construct() {
         $this->DB_connection = new Database_Connection();
     }
 
     /**
-     * Method/function name: validate_User()
-     * Description: a User_Control method which is intented to query the database and determine if the given password for said user is current.
+     * Method: Given a password/username fetched from the login form, checks to ensure the password is correct and if true then sets up the session variables
+     * 
+     * @param string $user
+     * @param string $password
      * @return boolean
      */
     public function validate_User($user, $password) {
@@ -39,6 +36,11 @@ Class User_Control {
         return false;
     }
     
+    /**
+     * Method: if the validate_user function returns true, calls generate_session_id to generate a 15 character session key.
+     * 
+     * @return type
+     */
     private function generate_session_id(){
         $randomIntString = "";
         for($i=0;$i<15;$i++){
@@ -47,6 +49,9 @@ Class User_Control {
         return $randomIntString;
     }
     
+    /**
+     * Method: destroy session and delete the sessionHash column in the users table
+     */
     public function destroy_session(){
         if(isset($_SESSION['sessionHash'])){
             $this->DB_connection->Prepared_query_Database("UPDATE users SET sessionHash=?", array(""), array("s"));
@@ -56,8 +61,8 @@ Class User_Control {
     }
 
     /**
-     * Method/function name: is_Session_Active()
-     * Description:  When ever a php file is loaded, must ensure that A. the session_start() is present and that B. The session varaible "user_ID" is set, if not then it assumes that the user has logged out and will redirect to
+     * 
+     * Method:  When ever a php file is loaded, must ensure that A. the session_start() is present and that B. The session varaible "user_ID" is set, if not then it assumes that the user has logged out and will redirect to
      * the login page
      * @return boolean Returns true if the session is active and the key: "user_ID" is set, false otherwise
      */
@@ -73,7 +78,9 @@ Class User_Control {
             else{return false;}
         } else {return false; }
     }
-// come back too
+    
+
+    /*
     public function set_Cohort(){
         $query = "select cohort,semester from students order by cohort desc, semester desc limit 1";
         $result = $this->DB_connection->query_Database($query);
@@ -82,5 +89,5 @@ Class User_Control {
             $update_query = "update users set cohort=".$row['cohort'].",semester=".$row['semester'];
             $this->DB_connection->query_Database($update_query);
         }
-    }
+    }*/
 }
