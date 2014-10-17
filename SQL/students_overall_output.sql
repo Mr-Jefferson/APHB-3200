ac4a550a26bc8fc17744ffb91129a4a7d7d5e7ea
Partly Lukerative <mental_monkey2004@hotmail.com>
2014-10-17 15:40:40 +0800

    Update students_overall_output.sql

--- SQL/students_overall_output.sql
+++ SQL/students_overall_output.sql
@@ -8,12 +8,12 @@ CREATE VIEW students_overall_output AS
 SELECT 	student_last_name,
 		student_first_name,
         student_number,
-		TRUNCATE(AVG(proposal_marks.mark_1)*0.5 + AVG(proposal_marks.mark_2)*0.5,2) AS proposal_mark_1_2_average,
-        TRUNCATE(AVG(proposal_marks.mark_3)*1.0,2) AS proposal_mark_3,
-        TRUNCATE(AVG(proposal_marks.mark_1)*0.1 + AVG(proposal_marks.mark_2)*0.1 + AVG(proposal_marks.mark_3)*0.8,2)*10 AS proposal_mark_1_2_3_weighted,
-        TRUNCATE(AVG(final_marks.mark_1)*0.5 + AVG(final_marks.mark_2)*0.5,2) AS final_mark_1_2_average,
-        TRUNCATE(AVG(final_marks.mark_3)*1.0,2) AS final_mark_3,
-        TRUNCATE(AVG(final_marks.mark_1)*0.1 + AVG(final_marks.mark_2)*0.1 + AVG(final_marks.mark_3)*0.8,2)*10 AS final_mark_1_2_3_weighted,
+		ROUND(AVG(proposal_marks.mark_1)*0.5 + AVG(proposal_marks.mark_2)*0.5,2) AS proposal_mark_1_2_average,
+        ROUND(AVG(proposal_marks.mark_3)*1.0,2) AS proposal_mark_3,
+        ROUND(AVG(proposal_marks.mark_1)*0.1 + AVG(proposal_marks.mark_2)*0.1 + AVG(proposal_marks.mark_3)*0.8,2)*10 AS proposal_mark_1_2_3_weighted,
+        ROUND(AVG(final_marks.mark_1)*0.5 + AVG(final_marks.mark_2)*0.5,2) AS final_mark_1_2_average,
+        ROUND(AVG(final_marks.mark_3)*1.0,2) AS final_mark_3,
+        ROUND(AVG(final_marks.mark_1)*0.1 + AVG(final_marks.mark_2)*0.1 + AVG(final_marks.mark_3)*0.8,2)*10 AS final_mark_1_2_3_weighted,
         students.cohort,
         students.semester
 FROM	students
