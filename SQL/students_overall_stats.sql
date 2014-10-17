c111eb2a8793e6a11f6df71883cfe9587c6ff832
Partly Lukerative <mental_monkey2004@hotmail.com>
2014-10-17 15:41:29 +0800

    TRUNCATE TO ROUND

--- SQL/students_overall_stats.sql
+++ SQL/students_overall_stats.sql
@@ -5,11 +5,11 @@ USE seminar_marks;
 DROP VIEW IF EXISTS students_overall_stats;
 
 CREATE VIEW students_overall_stats AS
-(SELECT	TRUNCATE(AVG(proposal_total),2) AS overall_average,
+(SELECT	ROUND(AVG(proposal_total),2) AS overall_average,
 		MAX(proposal_total) AS overall_maximum,
 		MIN(proposal_total) AS overall_minimum,
 		(MAX(proposal_total)-MIN(proposal_total)) AS overall_range,
-		TRUNCATE(STDDEV(proposal_total),2) AS overall_stddev,
+		ROUND(STDDEV(proposal_total),2) AS overall_stddev,
 		students.cohort,
 		students.semester,
 		1 AS seminar
@@ -17,11 +17,11 @@ FROM	students INNER JOIN
 		students_overall ON students_overall.id_student = students.id_student
 GROUP BY semester, cohort)
 UNION
-(SELECT	TRUNCATE(AVG(final_total),2) AS overall_average,
+(SELECT	ROUND(AVG(final_total),2) AS overall_average,
 		MAX(final_total) AS overall_maximum,
 		MIN(final_total) AS overall_minimum,
 		(MAX(final_total)-MIN(final_total)) AS overall_range,
-		TRUNCATE(STDDEV(final_total),2) AS overall_stddev,
+		ROUND(STDDEV(final_total),2) AS overall_stddev,
 		students.cohort,
 		students.semester,
 		2 AS seminar
