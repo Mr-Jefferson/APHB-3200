550a8d2cd4f38f06316a82755c74106d19b16c2d
nick <nick@nick-VirtualBox>
2014-10-17 15:44:18 +0800

    Final

--- SQL/markers_individual.sql
+++ SQL/markers_individual.sql
@@ -5,7 +5,7 @@ USE seminar_marks;
 DROP VIEW IF EXISTS markers_individual;
 
 CREATE VIEW markers_individual AS
-SELECT	TRUNCATE(AVG(proposal_marks.mark_1),2) AS proposal_mark_1_average,
+SELECT	ROUND(AVG(proposal_marks.mark_1),2) AS proposal_mark_1_average,
         TRUNCATE(AVG(proposal_marks.mark_2),2) AS proposal_mark_2_average,
         TRUNCATE(AVG(proposal_marks.mark_3),2) AS proposal_mark_3_average,
         TRUNCATE(AVG(proposal_marks.mark_1)*0.1 + AVG(proposal_marks.mark_2)*0.1 + AVG(proposal_marks.mark_3)*0.8,2)*10 AS proposal_overall_average,
@@ -21,5 +21,6 @@ FROM 	marks AS proposal_marks INNER JOIN
 		marks AS final_marks ON proposal_marks.id_marker = final_marks.id_marker INNER JOIN
         students ON proposal_marks.id_student = students.id_student AND final_marks.id_student = students.id_student
 WHERE   proposal_marks.seminar = 1 AND
-		final_marks.seminar = 2
+		final_marks.seminar = 2 AND
+        proposal_marks.id_marker = final_marks.id_marker
 GROUP BY proposal_marks.id_marker, final_marks.id_marker;
